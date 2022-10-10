bf3566f242c62bc91e778b4b99f8b3aa7ad23a98
sdong4884 <sdong4884@gmail.com>
2022-10-10 18:59:19 +0900

    useAsync 커스텀 Hook 만들어서 사용하기

--- src/Users.js
+++ src/Users.js
@@ -1,71 +1,36 @@
 import axios from "axios";
-import React, { useEffect, useReducer } from "react";
+import React, { useState } from "react";
+import useAsync from "./useAsync";
+import User from "./User";
 
-function reducer (state, action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null
-      }
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null
-      }
-    case 'ERROR':
-      return {
-        loading: false,
-        data: null,
-        error: action.error
-      }
-    default: 
-      throw new Error(`Unhandled action type: ${action.type}`)
-  }
+async function getUsers () {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+  return response.data
 }
 
 function Users () {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    date: null,
-    error: null
-  })
-
-  const fetchUsers = async () => {
-    dispatch({ type: 'LOADING' })
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/users')
-      dispatch({
-        type: 'SUCCESS',
-        data: response.data
-      })
-    } catch (e) {
-      dispatch({
-        type: 'ERROR',
-        error: e
-      })
-    }
-  }
-
-  useEffect(() => {
-    fetchUsers()
-  }, [])
+  const [state, refetch] = useAsync(getUsers, [], true)
+  const [userId, setUserId] = useState(null)
 
   const { loading, data: users, error } = state
   if (loading) return <div>로딩중 ...</div>
   if (error) return <div>에러가 발생했습니다</div>
-  if (!users) return null
+  if (!users) return <button onClick={refetch}>불러오기</button>
 
   return (
-    <ul>
-      {users.map(user =>
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>  
-      )}
-    </ul>
+    <>
+      <ul>
+        {users.map(user =>
+          <li 
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+          >
+            {user.username} ({user.name})
+          </li>  
+        )}
+      </ul>
+      { userId && <User id={userId} />}
+    </>
   )
 }
 
